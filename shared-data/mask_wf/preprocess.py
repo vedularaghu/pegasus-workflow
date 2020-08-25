7e04f80391cb11d81dd93a94cc9be4ecac020104
Raghavendra Vedula <rvedula@usc.edu>
2020-08-25 11:43:44 -0700

    train_model update

--- shared-data/mask_wf/preprocess.py
+++ shared-data/mask_wf/preprocess.py
@@ -20,8 +20,8 @@ check = [i for i in mask if "mask" in i]
 testing_files = set(os.listdir(image_path)) & set(os.listdir(mask_path))
 training_files = check
 
-new_image_path = "./norm_images/"
-new_mask_path = "./norm_masks/"
+# new_image_path = "./norm_images/"
+# new_mask_path = "./norm_masks/"
 
 os.mkdir(new_image_path)
 os.mkdir(new_mask_path)
@@ -35,7 +35,7 @@ for i in training_files:
     mask = cv2.resize(cv2.imread(os.path.join(mask_path,i+".png")),(X_shape,X_shape))[:,:,0]    
     final_img = cv2.normalize(im,  norm_img, 0, 255, cv2.NORM_MINMAX)
     final_mask = cv2.normalize(mask, norm_img, 0, 255, cv2.NORM_MINMAX)
-    cv2.imwrite(os.path.join(new_image_path,i.split("_mask")[0]+"_norm.png"), final_img)
-    cv2.imwrite(os.path.join(new_mask_path,i+"_norm.png"), final_mask)
+    cv2.imwrite(os.path.join(image_path,i.split("_mask")[0]+"_norm.png"), final_img)
+    cv2.imwrite(os.path.join(mask_path,i+"_norm.png"), final_mask)
     im_array.append(final_img)
     mask_array.append(final_mask)
