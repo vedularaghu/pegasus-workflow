90dc8934f83f2cf382d1dd18f27d256546af145a
Raghavendra Vedula <rvedula@usc.edu>
2020-09-16 15:03:44 -0700

    UNet using keras

--- shared-data/mask_wf/preprocess.py
+++ shared-data/mask_wf/preprocess.py
@@ -5,7 +5,7 @@ import numpy as np
 from cv2 import imread 
 
 DIR = "."
-X_shape = 256*2
+X_shape = 256
 norm_img = np.zeros((800,800))
 
 class DataPreprocessing:    
@@ -16,12 +16,16 @@ class DataPreprocessing:
         final_img = cv2.normalize(im,  norm_img, 0, 255, cv2.NORM_MINMAX)
         
         return final_img
+    
+def main():
+    files = os.listdir(DIR)
+    images = [i for i in files if ".png" in i]
 
-files = os.listdir(DIR)
-images = [i for i in files if ".png" in i]
+    dp = DataPreprocessing()
 
-dp = DataPreprocessing()
-
-for i in images:        
-    normalized_image = dp.normalize(i)
-    cv2.imwrite(i.split(".png")[0]+"_norm.png", normalized_image)
+    for i in images:        
+        normalized_image = dp.normalize(i)
+        cv2.imwrite(i.split(".png")[0]+"_norm.png", normalized_image)
+        
+if __name__ == "__main__":
+    main()
